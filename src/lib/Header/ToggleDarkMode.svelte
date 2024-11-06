7af4557beb1c9ba48403aaa80b20c5899bf3c209
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2024-11-06 23:36:59 +0000

    Update ToggleDarkMode.svelte

--- src/lib/Header/ToggleDarkMode.svelte
+++ src/lib/Header/ToggleDarkMode.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { theme } from '$lib/runes/theme.svelte';
 	import { icons } from '$lib/Icons/Icons';
+	import Moon from '$lib/Icons/Moon.svelte';
+	import BloodDrop from '$lib/Icons/BloodDrop.svelte';
+	import Sun from '$lib/Icons/Sun.svelte';
 
 	function changeTheme() {
 		if (theme.state == 'light') {
@@ -16,6 +19,23 @@
 	const SvelteComponent = $derived(icons[theme.state]);
 </script>
 
+<div class="bg-skin-secondary mr-2 flex h-8 w-8 items-center justify-center rounded-full">
+	<button
+		class="fill-base stroke-base hover:stroke-hover hover:fill-hover h-5 w-5 rounded-full"
+		aria-label="Toggle Theme"
+		onclick={changeTheme}
+	>
+		{#if theme.state == 'light'}
+			<Sun />
+		{/if}
+		{#if theme.state == 'dark'}
+			<Moon />
+		{/if}
+		{#if theme.state == 'blood'}
+			<BloodDrop />
+		{/if}
+	</button>
+</div>
 <div class="bg-skin-secondary mr-2 flex h-8 w-8 items-center justify-center rounded-full">
 	<button
 		class="fill-base stroke-base hover:stroke-hover hover:fill-hover h-5 w-5 rounded-full"
