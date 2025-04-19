ac177e762dbd106ca8e6ddddbc5ddf8dfb9615e7
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-04-19 02:55:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-// import { readFileSync } from 'node:fs';
-// import { fileURLToPath } from 'node:url';
-
-// const path = fileURLToPath(new URL('package.json', import.meta.url));
-// const pkg = JSON.parse(readFileSync(path, 'utf8'))
-
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	// define: {
-	// 	__VERSION__: JSON.stringify(pkg.version)
-	// }
 });
