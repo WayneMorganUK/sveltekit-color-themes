874c49ac26836c779131295fa51e20a25c65b08d
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-12-07 01:31:12 +0000

    added platform specific adapter logic

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+let selectedAdapter = adapterAuto;
+
+if (process.env.VERCEL === '1') {
+	selectedAdapter = adapterVercel();
+} else if (process.env.CF_PAGES === '1') {
+	selectedAdapter = adapterCloudflare();
+}
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: selectedAdapter()
 	}
 };
 
