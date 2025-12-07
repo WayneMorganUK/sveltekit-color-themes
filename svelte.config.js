bf1e6595796e9fd58e192d0ff8f5d2dd685fb224
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-12-07 01:40:49 +0000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,22 @@
-import adapterAuto from '@sveltejs/adapter-auto';
-import adapterVercel from '@sveltejs/adapter-vercel';
+// import adapterAuto from '@sveltejs/adapter-auto';
+// import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-let selectedAdapter = adapterAuto;
+console.log('Using Cloudflare adapter', process.env);
+// let selectedAdapter = adapterAuto;
 
-if (process.env.VERCEL === '1') {
-	selectedAdapter = adapterVercel();
-} else if (process.env.CF_PAGES === '1') {
-	selectedAdapter = adapterCloudflare();
-}
+// if (process.env.VERCEL === '1') {
+// 	selectedAdapter = adapterVercel();
+// } else if (process.env.CF_PAGES === '1') {
+// 	selectedAdapter = adapterCloudflare();
+// }
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: selectedAdapter()
+		adapter: adapterCloudflare()
 	}
 };
 
