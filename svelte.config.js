17dd1a958b43339117681b9d33772fdf38f11bf3
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-12-09 12:59:59 +0000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 // import adapterAuto from '@sveltejs/adapter-auto';
-// import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 let adapter = adapterCloudflare;
-// let options = {};
+let options = {};
 
 // console.log(
 // 	'Process env Cloudflare workers = ',
@@ -17,24 +17,22 @@ let adapter = adapterCloudflare;
 // );
 // console.log('Process env Vercel = ', process.env.VERCEL ?? 'not set');
 
-// if (process.env.VERCEL == '1') {
-// 	adapter = adapterVercel;
-// } else if (process.env.WORKERS_CI == '1') {
-// 	adapter = adapterCloudflare;
-// 	// options = { config: 'cf_workers/wrangler.jsonc' };
-// } else if (process.env.CF_PAGES == '1') {
-// 	adapter = adapterCloudflare;
-// 	// options = { config: 'cf_pages/wrangler.toml' };
-// }
+if (process.env.VERCEL == '1') {
+	adapter = adapterVercel;
+} else if (process.env.WORKERS_CI == '1') {
+	adapter = adapterCloudflare;
+	options = { config: 'wrangler.jsonc' };
+} else if (process.env.CF_PAGES == '1') {
+	adapter = adapterCloudflare;
+	options = { config: 'wrangler.toml' };
+}
 // console.log(options);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			config: './config/wrangler.jsonc'
-		})
+		adapter: adapter(options)
 	}
 };
 
