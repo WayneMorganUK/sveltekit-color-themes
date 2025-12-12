4acea68ab05a782e5bb4f1f09020c8671c266eeb
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-12-12 12:33:53 +0000

    updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # A Sveltekit App using Color Themes
 
-## Uses logic to deploy to Vercel, CF Pages and Cf Workers
+## Uses logic to deploy to Vercel, Netlify, CF Pages and Cf Workers
 
 
 If using pnpm and deploying to cloudflare workers in the build settings change default deploy command
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,43 @@
-// import adapterAuto from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-let adapter = adapterCloudflare;
+// import { BYPASS_TOKEN } from '$env/static/private';
+
+let adapter = adapterAuto;
 let options = {};
+
+console.log(process.env);
 
 if (process.env.VERCEL == '1') {
 	adapter = adapterVercel;
+	options = {
+		// runtime: 'edge',
+		// split: true,
+		// memory: 128,
+		// isr: {
+		// 	expiration: 60,
+		// 	bypassToken: BYPASS_TOKEN,
+		// 	allowQuery: ['search']
+		// }
+	};
 } else if (process.env.WORKERS_CI == '1') {
 	adapter = adapterCloudflare;
-	options = { config: 'cf_workers/wrangler.jsonc' };
+	options = {
+		config: 'cf_workers/wrangler.jsonc',
+		platformProxy: {
+			configPath: undefined,
+			environment: undefined,
+			persist: undefined
+		},
+		fallback: 'plaintext',
+		routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		}
+	};
 
 	// **** NOTE: add  pnpx wrangler deploy --config ./cf_workers/ to build command in Cloudflare  *****
 } else if (process.env.CF_PAGES == '1') {
