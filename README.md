15985a06e8931d5bf5fd023bad1e8adbff362dcb
Wayne Morgan <44246461+WayneMorganUK@users.noreply.github.com>
2025-12-12 13:12:47 +0000

    Updates

--- README.md
+++ README.md
@@ -2,11 +2,24 @@
 
 ## Uses logic to deploy to Vercel, Netlify, CF Pages and Cf Workers
 
-
-If using pnpm and deploying to cloudflare workers in the build settings change default deploy command
+If using pnpm and deploying to Cloudflare Workers, in the build settings change default deploy command
 from `npx wrangler deploy` to `pnpx wrangler deploy`
 
-##Build configuration
+## Build configuration
 Build command `pnpm run build`
-Deploy command `pnpx wrangler deploy`
+Deploy command `pnpx wrangler deploy --config ./cf_workers/`
 
+**** NOTE: add  pnpx wrangler deploy --config ./cf_workers/ to build command in Cloudflare or the relavnt location of your wrangler.jsonc file  *****
+
+
+## Checks to see which platform you are deploying to using the following conditions:
+process.env.VERCEL === '1'
+process.env.NETLIFY === 'TRUE'
+process.env.CF_PAGES ==='1'
+process.env.CF_WORKERS_CI === '1'
+
+For more info refer to the relevant documents
+Adapters: https://svelte.dev/docs/kit/adapters
+Cloudflare Pages and Workers: https://svelte.dev/docs/kit/adapter-cloudflare
+Netlify: https://svelte.dev/docs/kit/adapter-netlify
+Vercel: https://svelte.dev/docs/kit/adapter-vercel
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
+// import { BYPASS_TOKEN } from '$env/static/private';
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// import { BYPASS_TOKEN } from '$env/static/private';
-
 let adapter = adapterAuto;
 let options = {};
 
-console.log(process.env);
+// console.log(process.env);
 
-if (process.env.VERCEL == '1') {
+// ** Vercel Adapter and Options **
+if (process.env.VERCEL === '1') {
 	adapter = adapterVercel;
 	options = {
 		// runtime: 'edge',
@@ -23,28 +23,44 @@ if (process.env.VERCEL == '1') {
 		// 	allowQuery: ['search']
 		// }
 	};
-} else if (process.env.WORKERS_CI == '1') {
+} else if (process.env.WORKERS_CI === '1') {
+	// ** Cloudflare Workers Adapter and Options **
 	adapter = adapterCloudflare;
 	options = {
-		config: 'cf_workers/wrangler.jsonc',
-		platformProxy: {
-			configPath: undefined,
-			environment: undefined,
-			persist: undefined
-		},
-		fallback: 'plaintext',
-		routes: {
-			include: ['/*'],
-			exclude: ['<all>']
-		}
+		config: 'cf_workers/wrangler.jsonc'
+		// platformProxy: {
+		// 	configPath: undefined,
+		// 	environment: undefined,
+		// 	persist: undefined
+		// },
+		// fallback: 'plaintext',
+		// routes: {
+		// 	include: ['/*'],
+		// 	exclude: ['<all>']
+		// }
 	};
 
 	// **** NOTE: add  pnpx wrangler deploy --config ./cf_workers/ to build command in Cloudflare  *****
-} else if (process.env.CF_PAGES == '1') {
+} else if (process.env.CF_PAGES === '1') {
+	// ** Cloudflare Pages Adapter and Options **
 	adapter = adapterCloudflare;
 	// *** NOTE: Cloudflare pages has to use the root directory
+} else if (process.env.NETLIFY === 'TRUE') {
+	// ** Netlify Adapter and Options **
+	adapter = adapterNetlify;
+	options = {
+		// if true, will create a Netlify Edge Function rather
+		// than using standard Node-based functions
+		edge: false,
+
+		// if true, will split your app into multiple functions
+		// instead of creating a single one for the entire app.
+		// if `edge` is true, this option cannot be used
+		split: false
+	};
 }
-// console.log(options);
+
+console.log(options);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
